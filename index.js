077485cb311be913fbb48c140c655279dbca849e
nyambujoy <joyliznyambura@gmail.com>
2025-06-09 15:33:50 +0300

    used express

--- index.js
+++ index.js
@@ -1,57 +1,33 @@
 //server
 console.log('hello');
-import http from 'http'
-import fs from 'fs/promises'
-import path from 'path';
-import url from 'url';
 
+
+const express = require('express');
+const path = require('path');
+const app = express()
 const PORT = 8080;
 
-const __fileName = url.fileURLToPath(import.meta.url);
-const __dirName = path.dirname(__fileName);
+// app.use(express.static(path.join(__dirname, 'public')))
 
-const server = http.createServer(async (req, res) => {
-
-
-    try {
-
-        if (req.method === 'GET') {
-
-            let filePath;
-
-            if (req.url === '/') {
-                filePath = path.join(__dirName, 'index.html')
-            } else if (req.url === '/about') {
-                filePath = path.join(__dirName, 'about.html')
-
-            } else if (req.url === '/contact-me') {
-                filePath = path.join(__dirName, 'contact-me.html')
-
-            } else {
-                filePath = path.join(__dirName, '404.html');
-                res.statusCode = 404;
-
-            }
-
-            const data = await fs.readFile(filePath);
-            res.setHeader('Content-Type', 'text/html');
-            res.write(data);
-            res.end()
-        } else {
-            throw new Error('method not allowed!')
-        }
-
-    } catch (error) {
-        res.statusCode = 500;
-        res.setHeader('Content-Type', 'text/html');
-        res.end('Server error');
-
-    }
-
-
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'index.html'))
 })
 
-server.listen(PORT, () => {
-    console.log(`server is running on port ${PORT}`)
+app.get('/about', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'about.html'))
+});
+
+app.get('/contact', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'contact-me.html'))
+});
+
+//404 handler
+app.use((req, res) => {
+    res.status(404).sendFile(path.join(__dirname, 'public', '404.html'));
 })
+
+
+
+
+app.listen(PORT, () => { console.log(`${PORT} is running`) });
 
